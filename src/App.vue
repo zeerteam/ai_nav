4dd65a0c67221a8f8ea01d41f08e2f13b5d96c83
zeer <boxit@qq.com>
2024-02-16 21:41:23 +0800

    commit all

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, toRef } from 'vue';
+import Unit from './components/Unit.vue'
+import info from './assets/datas.json'
+const datas = ref({})
+// fetch("https://ai.zeer.xyz/assets/datas.json")
+//   .then(res => res.json())
+//   .then(data => {
+//     datas.value = data;
+//   })
+//   .catch(e => console.log(e));
+datas.value = info;
+</script>
+
+<template>
+  <div class="nav_title">
+    <div>
+      国内
+    </div>
+    <div>
+      国外
+    </div>
+    <div>
+      工具
+    </div>
+    <div>
+      其它
+    </div>
+  </div>
+  <div class="nav">
+    <div v-for="i in datas" :key="i">
+      <div v-for="item in i" :key="item">
+        <Unit :unit="item" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav_title {
+  line-height: 3em;
+  font-size: 1.4em;
+  font-weight: 600;
+  background: #7535bb;
+  position: sticky;
+  z-index: 999;
+  top: 0;
+}
+
+.nav>div,
+.nav_title>div {
+  display: inline-block;
+  width: 25%;
+  overflow: hidden;
+  vertical-align: top;
+  text-align: center;
+}
+</style>
---
+++ src/components/Unit.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="unit">
+    <a :href="url" target="_blank" :title="title">
+      <img class="logo" :title="title" :src="logo" />
+      <p class="title">{{ title }}</p>
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from 'vue';
+const props = defineProps({
+  unit: Object,
+})
+const { logo, url, title } = toRefs(props.unit)
+</script>
+
+<style scoped>
+.unit {
+  margin-bottom: 50px;
+  margin-top: 20px;
+}
+
+.unit:hover {
+  background: rgba(20, 20, 20, .3);
+}
+
+.logo {
+  max-height: 80px;
+  max-width: 120px;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.title {
+  margin-top: 0.2em;
+  font-weight: 600;
+  font-size: 1.2em;
+}
+</style>
